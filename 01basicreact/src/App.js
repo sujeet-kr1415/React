22e4170dd1f13916e726052da72b56526343de8d
Sujeet Kumar <sujeet.kr1415@gmail.com>
2023-12-26 01:49:58 +0530

    project with react and vite react

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>sujeet and react</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+
+function App() {
+  
+  return (
+    <h1>sujeet and react with vite</h1>
+  )
+}
+
+export default App
